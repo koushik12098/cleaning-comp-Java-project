0e2144be59ccbd51f01ae4b9ebe8173bdcc3aa9a
koushik12098 <125720042+koushik12098@users.noreply.github.com>
2023-04-18 13:23:34 +0530

    Add files via upload

---
+++ Cleaner.java
@@ -0,0 +1,39 @@
+public class Cleaner {
+    private String name;
+    private String address;
+    private String phone;
+    private String email;
+
+    public Cleaner(String name, String address, String phone, String email) {
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public String toString() {
+        return "Cleaner{" +
+                "name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", phone='" + phone + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
